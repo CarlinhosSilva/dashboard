999ba4aef61ba2ddb2972a75853fe1b07e016ec2
Carlinhos Silva <carlinhosssilva87@gmail.com>
2024-09-20 23:10:23 -0300

    change env var on github yml

--- tests/setup.js
+++ tests/setup.js
@@ -1,12 +1,22 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
+let mongoServer;
+
 beforeAll(async () => {
-    // Conectar ao banco de dados de teste
-    await mongoose.connect(process.env.MONGO_TEST_URI);
-    console.log('Conectado ao banco de dados de teste');
+    // Iniciar o MongoDB em memória
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+
+    // Conectar ao banco de dados em memória
+    await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log('Conectado ao banco de dados em memória');
 });
 
 afterEach(async () => {
@@ -18,7 +28,8 @@ afterEach(async () => {
 });
 
 afterAll(async () => {
-    // Desconectar após os testes
-    await mongoose.connection.dropDatabase();
-    await mongoose.connection.close();
+    // Desconectar após os testes e parar o servidor em memória
+    await mongoose.disconnect();
+    await mongoServer.stop();
+    console.log('Desconectado do banco de dados em memória');
 });
